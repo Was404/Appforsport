e70cb94fce00ebfc345e98a2277d82fcd181e3e0
Was404 <vladimirovichdanil@bk.ru>
2023-01-16 17:55:14 +0300

    background game and btn style

--- app/src/main/java/com/example/appforsport/GameActivity.java
+++ app/src/main/java/com/example/appforsport/GameActivity.java
@@ -2,27 +2,21 @@ package com.example.appforsport;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-public class GameActivity extends AppCompatActivity implements View.OnTouchListener {
+public class GameActivity extends AppCompatActivity {
 
     ImageButton btn_back, btn_left, btn_right;
-    ConstraintLayout game;
 
-    public static boolean isLeftOn = false;
-    public static boolean isRightOn = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_main);
-        GameView gameView = new GameView(this); //класс создай
-        game = (ConstraintLayout) findViewById(R.id.game);
+          //класс создай
         btn_back = (ImageButton) findViewById(R.id.btn_back);
         btn_left = (ImageButton) findViewById(R.id.btn_left);
         btn_right = (ImageButton) findViewById(R.id.btn_right);
@@ -34,30 +28,5 @@ public class GameActivity extends AppCompatActivity implements View.OnTouchListe
                 startActivity(intent);
             }
         });
-        btn_left.setOnTouchListener(this);
-        btn_left.setOnTouchListener(this);
-
-    }
-
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        switch (ImageButton.getId()){
-            case R.id.btn_left:
-                switch (motionEvent.getAction()){
-                    case MotionEvent.ACTION_DOWN:
-                        isLeftOn = true;
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        isLeftOn = false;
-                        break;
-                }
-                break;
-            case R.id.btn_right:
-                switch (motionEvent.getAction()){
-                    case MotionEvent.ACTION_DOWN:
-
-                }
-        }
-        return false;
     }
 }
